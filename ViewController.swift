dcb841f76327994397dd3e0dd30e2ee41cfccd31
Michał Januszewski <blackrainpl@gmail.com>
2020-02-12 14:29:42 +0100

    Basic project structure. Removed storyboards.

--- ViewController.swift
+++ ViewController.swift
@@ -8,5 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {}
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .lightGray
+    }
+}
 
